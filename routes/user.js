b9134fd1835012df256a1ae1de3dada282437e6e
Anuj Rishu TIwari <at6272@srmist.edu.in>
2024-04-23 09:38:02 +0530

    added authz part

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,31 @@
 const express =require("express")
 const router =express.Router();
+const {auth , isStudent , isAdmin} =require("../middlewares/auth")
 const {login, signup} = require("..//controllers/auth")
-// router.post("/login" ,login)
+router.post("/login" ,login)
 router.post("/signup" ,signup);
+
+
+// testing route
+router.get("/test" , auth , (req, res)=>{
+    res.json({
+        success: true,
+        message: "welcome to test page"
+    })
+})
+//protected routes
+router.get("/student" , auth ,isStudent , (req, res)=>{
+    res.json({
+        success: true,
+        message: "welcome to sudent page"
+    })
+})
+router.get("/admin" , auth ,isAdmin , (req, res)=>{
+    res.json({
+        success: true,
+        message: "welcome to Admin page"
+    })
+})
+
+
 module.exports = router;
